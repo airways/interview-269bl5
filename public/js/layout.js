88aa4f3e15460060500289d7912fa9d2a58c7d5a
Isaac Raway <isaac@mm.st>
2022-06-15 16:40:59 -0500

    Removed extra border line from payments sub-table in invoice report; added sendReminderEmail click handler for Send reminder email button and onload handler to bind it

--- public/js/layout.js
+++ public/js/layout.js
@@ -20,3 +20,51 @@ window.showAlert = (message, type) => {
         alertContainer.replaceChildren(wrapper);
     }
 };
+
+/**
+ * After page completes loading, bind UI handlers and finalize UI state for the reports page.
+ *
+ * @param {Event} event
+ */
+window.onload = function() {
+    // Send reminder email buttons are disabled initially so the user doesn't click
+    // them before we can bind the event handler here. Enable the buttons and bind the
+    // events.
+    const buttons = document.getElementsByClassName('send-reminder-button');
+    for(var i = 0, buttons_length = buttons.length; i < buttons_length; ++i) { // Prevent repeated length evaluation
+        buttons[i].removeAttribute('disabled');
+        buttons[i].onclick = sendReminderEmail;
+    }
+}
+
+/**
+ * Send a request to the server for a reminder email to be transmitted to the contact.
+ *
+ * @param {MouseEvent} event
+ */
+function sendReminderEmail(event) {
+    const invoiceId = event.target.getAttribute('data-invoice_id');
+    if(invoiceId) {
+        fetch('/reports/sendReminder/'+invoiceId, {
+                method: 'POST',
+            })
+            .then(function(response) {
+                if(!response.ok) {
+                    window.showAlert('Reminder sent. Internal server error. If this condition persists please contact support.', 'danger');
+                } else {
+                    return response.json();
+                }
+            })
+            .then(function(data) {
+                if(data.error) {
+                    window.showAlert('Reminder not sent. ' + data.message, 'danger');
+                } else {
+                    window.showAlert('Reminder sent.', 'success');
+                }
+            }).catch(function(error) {
+                window.showAlert('Reminder not sent. You may be offline or there may be an internal server error, please try again later. If this condition persists please contact support.', 'danger');
+            })
+    } else {
+        window.showAlert('Reminder not sent. Internal client error. If this condition persists please contact support.', 'danger');
+    }
+}
